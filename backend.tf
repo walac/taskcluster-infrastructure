3e4a8d7b9d2f27722e5a81c2a496ff156c7fb308
Wander Lairson Costa <wander.lairson@gmail.com>
2020-04-29 10:51:02 -0300

    Remove azure as a state backend

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    storage_account_name = "tcterraformstate"
-    container_name       = "tfstate"
-    key                  = "state-test2.tf"
-
-    // NOTE: this requires ARM_ACCESS_KEY environment variable to be set
-  }
-}
